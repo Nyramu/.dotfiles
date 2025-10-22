9064071cef1519db394992630cd6f98391d32d21
Nyramu <runamu05@gmail.com>
2025-10-22 16:36:15 +0200

    Feat: changed default terminal

--- nyramu/home-manager/desktops/hyprland/binds.nix
+++ nyramu/home-manager/desktops/hyprland/binds.nix
@@ -6,7 +6,7 @@
       MOUSE_L = "mouse:272";
       MOUSE_R = "mouse:273";
 
-      terminal = "${pkgs.alacritty}/bin/alacritty";
+      terminal = "ghostty";
       browser = "${pkgs.firefox}/bin/firefox";
       fileManager = "${pkgs.nautilus}/bin/nautilus";
 
