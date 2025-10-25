e67fc061ae390411cb93dd6e24918b86c90b1820
Nyramu <runamu05@gmail.com>
2025-10-25 17:59:14 +0200

    Added opacity settings

--- nyramu/nixos/stylix.nix
+++ nyramu/nixos/stylix.nix
@@ -16,5 +16,11 @@ in
     fonts = theme.fonts; 
     image = theme.background;
     polarity = theme.polarity;
+
+    opacity = {
+      terminal = 0.6;
+      applications = 0.7;
+      # I can apply it for desktop and popups
+    };
   };
 }
