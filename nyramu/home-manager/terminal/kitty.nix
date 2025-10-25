6fe88794e6494348d4d01d8403d4c316c28ac79a
Nyramu <runamu05@gmail.com>
2025-10-25 17:48:52 +0200

    Major changes

--- nyramu/home-manager/terminal/kitty.nix
+++ nyramu/home-manager/terminal/kitty.nix
@@ -1,12 +1,34 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  theme = import ../../aesthetics/themes/sunset.nix { inherit pkgs; };
+in
 {
   programs.kitty = {
     enable = true;
     enableGitIntegration = true;
-    themeFile = "IC_Orange_PPL";
-    # TODO 
-    # Set font
-  };
+    shellIntegration.enableZshIntegration = true;
+    settings = {
+      shell_integration = "enabled";
+      confirm_os_window_close = 0;
+      # Does not work for some reason
+      #startup_session = "/home/nyramu/.dotfiles";
+      allow_remote_control = "no";
+      listen_on = "none"; 
 
+      # Appearance
+      background_blur = 0;
+      #selection_background = "#${theme.palette.base02}";
+      #selection_foreground = "none";
+      #color8 = "#${theme.palette.base03}";
+      cursor_trail = 60;
+      cursor_trail_color = "#${theme.palette.base0A}";
+    };
+    # Fix for Stylix overriding normal settings
+    extraConfig = "
+      selection_background #${theme.palette.base02}
+      selection_foreground none
+      color8 #${theme.palette.base03} 
+    ";
+  };
 }
