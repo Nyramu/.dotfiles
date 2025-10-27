0b7526e60503ea75e73fb4f263aac784c1de9240
Nyramu <runamu05@gmail.com>
2025-10-28 00:43:02 +0100

    Rework: nyra options

--- nyra/home/apps/browsers/default.nix
+++ nyra/home/apps/browsers/default.nix
@@ -1,5 +1,24 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }: with lib;
 
+let
+  cfg = config.nyra.home.apps.browsers;
+in
 {
-  home.packages = with pkgs; [ firefox ];
+  options.nyra.home.apps.browsers = {
+    firefox = {
+      enable = mkEnableOption "firefox";
+    };
+    librewolf = {
+      enable = mkEnableOption "librewolf";
+    };
+  };
+
+  config = {
+    home.packages = optionals cfg.firefox.enable [pkgs.firefox];
+
+    programs.librewolf = {
+      enable = cfg.librewolf.enable;
+      # TODO: add librewolf config
+    };
+  };
 }
