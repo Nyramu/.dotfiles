c24d58f3e2d2e49df0fd0e3f383e0421fa5b3e75
Nyramu <runamu05@gmail.com>
2025-10-27 15:10:55 +0100

    Moved critical options to profiles/base

--- profiles/main/home.nix
+++ profiles/main/home.nix
@@ -1,20 +1,5 @@
 { pkgs, inputs, ... }:
 
 {
-  imports = [ ../../nyramu/home-manager/default.nix ];
-  
-
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowUnfreePredicate = (_ : true);
-  };
-
-  home = {
-    username = "nyramu";
-    homeDirectory = "/home/nyramu";
-    stateVersion = "25.11";
-    packages = with pkgs; [];
-  };
-
-  programs.home-manager.enable = true;
+  imports = [ ../base/home.nix ];
 }
