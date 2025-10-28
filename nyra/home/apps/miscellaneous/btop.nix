51c1c5ab3c55fc7128ea876d5e144d85e7227efa
Nyramu <runamu05@gmail.com>
2025-10-28 19:20:29 +0100

    Rework: added nyra option

--- nyra/home/apps/miscellaneous/btop.nix
+++ nyra/home/apps/miscellaneous/btop.nix
@@ -1,15 +1,24 @@
-{ pkgs, lib, ... }:
+{ config, lib, pkgs, ... }: with lib;
 
+let 
+  cfg = config.nyra.home.apps.miscellaneous;
+in 
 {
-  programs.btop = {
-    enable = true;
-    settings = {
-      color_theme = lib.mkForce "TTY";
-      theme_background = false;
-      force_tty = true;
-      update_ms = 100;
-      # TODO: Make gpu appear in btop
-      shown_boxes = "proc cpu mem net";
+  options.nyra.home.apps.miscellaneous = {
+    btop.enable = mkEnableOption "btop";
+  };
+
+  config = {
+    programs.btop = {
+      enable = cfg.btop.enable;
+      settings = {
+        color_theme = lib.mkForce "TTY";
+        theme_background = false;
+        force_tty = true;
+        update_ms = 1000;
+        # TODO: Make gpu appear in btop
+        shown_boxes = "proc cpu mem net";
+      };
     };
   };
 }
