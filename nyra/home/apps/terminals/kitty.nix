4a7d36a885a9efdfa60edf3de9845560d748a7ab
Nyramu <runamu05@gmail.com>
2025-10-28 19:38:15 +0100

    Rework: added nyra option

--- nyra/home/apps/terminals/kitty.nix
+++ nyra/home/apps/terminals/kitty.nix
@@ -1,34 +1,42 @@
-{ pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   theme = import ../../../../resources/themes/sunset.nix { inherit pkgs; };
+
+  cfg = config.nyra.home.apps.terminals;
 in
 {
-  programs.kitty = {
-    enable = true;
-    enableGitIntegration = true;
-    shellIntegration.enableZshIntegration = true;
-    settings = {
-      shell_integration = "enabled";
-      confirm_os_window_close = 0;
-      # Does not work for some reason
-      #startup_session = "/home/nyramu/.dotfiles";
-      allow_remote_control = "no";
-      listen_on = "none";
+  options.nyra.home.apps.terminals = {
+    kitty.enable = mkEnableOption "kitty";
+  };
 
-      scrollbar = "scrolled";
-      scrollbar_interactive = "yes";
-      scrollbar_jump_on_click = "yes";
-      scrollbar_handle_color = "#${theme.palette.base0A}";
-      background_blur = 0;
-      cursor_trail = 60;
-      cursor_trail_color = "#${theme.palette.base0A}";
+  config = {
+    programs.kitty = {
+      enable = cfg.kitty.enable;
+      enableGitIntegration = true;
+      shellIntegration.enableZshIntegration = config.nyra.home.shells.zsh.enable;
+      settings = {
+        shell_integration = "enabled";
+        confirm_os_window_close = 0;
+        # Does not work for some reason
+        #startup_session = "/home/nyramu/.dotfiles";
+        allow_remote_control = "no";
+        listen_on = "none";
+
+        scrollbar = "scrolled";
+        scrollbar_interactive = "yes";
+        scrollbar_jump_on_click = "yes";
+        scrollbar_handle_color = "#${theme.palette.base0A}";
+        background_blur = 0;
+        cursor_trail = 60;
+        cursor_trail_color = "#${theme.palette.base0A}";
+      };
+      # Fix for Stylix overriding normal settings
+      extraConfig = "
+        selection_background #${theme.palette.base02}
+        selection_foreground none
+        color8 #${theme.palette.base03} 
+      ";
     };
-    # Fix for Stylix overriding normal settings
-    extraConfig = "
-      selection_background #${theme.palette.base02}
-      selection_foreground none
-      color8 #${theme.palette.base03} 
-    ";
   };
 }
