dd1389f832654c1f03b8a03a6d0785e698daedf0
Manuel Maricca <107689027+Nyramu@users.noreply.github.com>
2025-11-01 22:13:14 +0100

    Update install.sh
    Removed flake.lock update

--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@
 
 sudo nixos-generate-config --show-hardware-config > ./profiles/main/hardware-configuration.nix
 
-nix flake update ~/.dotfiles
-
 sudo nixos-rebuild switch --flake ~/.dotfiles#system
 
 nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.dotfiles#user
