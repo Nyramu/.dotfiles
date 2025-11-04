051409feaf0bc73cf79cc1dcb3333b8c2970a836
Nyramu <runamu05@gmail.com>
2025-11-04 15:03:37 +0100

    updated nvf

--- nyra/home/apps/editors/nvf.nix
+++ nyra/home/apps/editors/nvf.nix
@@ -1,11 +1,10 @@
-{ config, lib, pkgs, inputs, ... }: with lib;
+{ config, lib, inputs, ... }: with lib;
 
 let
   cfg = config.nyra.home.apps.editors;
-in
-{
-  imports = [ inputs.nvf.homeManagerModules.default ];
-  
+in {
+  imports = [inputs.nvf.homeManagerModules.default];
+
   options.nyra.home.apps.editors.nvf = {
     enable = mkEnableOption "nvf";
   };
@@ -17,10 +16,39 @@ in
         vim.vimAlias = true;
         vim.lsp = {
           enable = true;
-	  formatOnSave = true;
-	  inlayHints.enable = true;
-	  lightbulb.enable = true;
-	  lspSignature.enable = true;
+          formatOnSave = false;
+          inlayHints.enable = true;
+          lightbulb.enable = true;
+          lspSignature.enable = true;
+        };
+        vim.languages = {
+          enableFormat = true;
+          enableTreesitter = true;
+          enableExtraDiagnostics = true;
+          nix = {
+            enable = true;
+            format.type = "alejandra";
+            extraDiagnostics.types = ["deadnix"];
+          };
+          #sql.enable = true;
+          #clang.enable = true;
+          #python.enable = true;
+          #markdown.enable = true;
+          #html.enable = true;
+          #php.enable = true;
+          #ts.enable = true;
+          #go.enable = true;
+          #lua.enable = true;
+        };
+        vim.options = {
+          scrolloff = 8;
+          tabstop = 2;
+          shiftwidth = 2;
+          wrap = true;
+          splitbelow = true;
+          splitright = true;
+          termguicolors = true;
+          cmdheight = 0;
         };
       };
     };
