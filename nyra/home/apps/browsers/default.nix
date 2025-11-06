a9d11f931c9b4cd9460ea3d1ce6c0dbcc67da55f
Nyramu <runamu05@gmail.com>
2025-11-07 00:08:15 +0100

    Added zen browser (does not work)

--- nyra/home/apps/browsers/default.nix
+++ nyra/home/apps/browsers/default.nix
@@ -5,11 +5,12 @@
     ./firefox.nix
     ./librewolf.nix
     ./qutebrowser.nix
+    ./zen.nix
   ];
 
   options.nyra.home.apps.browsers = {
     default = mkOption {
-      type = types.enum [ "firefox" "librewolf" "qutebrowser" ];
+      type = types.enum [ "firefox" "librewolf" "qutebrowser" "zen" ];
       default = "qutebrowser";
       description = "Choose your default browser";
     };
