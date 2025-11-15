83726d34cd5a56d4ce5448d2690c4519c49f89d7
Nyramu <runamu05@gmail.com>
2025-11-15 19:05:49 +0100

    Added cpu governor config

--- profiles/base/configuration.nix
+++ profiles/base/configuration.nix
@@ -73,6 +73,19 @@
     };
   };
 
+  # CPU Frequency
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
