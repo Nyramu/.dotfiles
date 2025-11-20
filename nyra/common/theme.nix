44341890f97764c6c7be6369f3e9583cd32dde9e
Nyramu <runamu05@gmail.com>
2025-11-21 00:18:27 +0100

    Added theme

--- nyra/common/theme.nix
+++ nyra/common/theme.nix
@@ -4,9 +4,9 @@
   options.nyra.theme = {
     enable = mkEnableOption "Enable theme";
     defaultTheme = mkOption {
-      type = types.enum [ "pixel-sunset" ];
+      type = types.enum [ "pixel-sunset" "red-eclipse" ];
       default = "pixel-sunset";
-      description = "Choose a theme: pixel-sunset";
+      description = "Choose a theme: pixel-sunset, red-eclipse";
     };
   };
 }
