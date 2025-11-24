72272583685c134d1614363f47c4c65ce13968c5
Nyramu <runamu05@gmail.com>
2025-11-24 01:50:30 +0100

    Added nixcord

--- flake.nix
+++ flake.nix
@@ -61,6 +61,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nixcord = {
+      url = "github:kaylorben/nixcord";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
