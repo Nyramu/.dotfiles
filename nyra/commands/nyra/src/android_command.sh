e3dfa5a8fd2cc022c96acd3c93c1a0ff058b0a98
Nyramu <runamu05@gmail.com>
2025-11-29 03:25:33 +0100

    Improved nyra android command

--- nyra/commands/nyra/src/android_command.sh
+++ nyra/commands/nyra/src/android_command.sh
@@ -5,10 +5,9 @@ unmount=${args[--unmount]}
 mkdir -p "$MOUNT_POINT"
 
 if [[ $unmount ]]; then
-  fusermount --unmount "$MOUNT_POINT"
-  cd
+  fusermount --unmount "$MOUNT_POINT" 2>/dev/null
+  echo "Device successfully unmounted"
 else
-  jmtpfs "$MOUNT_POINT"
+  jmtpfs "$MOUNT_POINT" 2>/dev/null
+  echo "Device successfully mounted in $MOUNT_POINT"
 fi
-
-cd ~/Android
