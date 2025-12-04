aea975fcc9cf6c7d0a172ab274f07f8b54f51d58
Nyramu <runamu05@gmail.com>
2025-12-04 22:59:14 +0100

    Removed davinci resolve

--- nyra/home/apps/editors/default.nix
+++ nyra/home/apps/editors/default.nix
@@ -9,13 +9,11 @@ in
   ];
 
   options.nyra.home.apps.editors = {
-    davinci.enable = mkEnableOption "davinci-resolve";
     gimp.enable = mkEnableOption "gimp";
   };
 
   config = {
     home.packages = with pkgs;
-      optionals (cfg.davinci.enable) [ davinci-resolve ] ++
       optionals (cfg.gimp.enable) [ gimp-with-plugins ];
   };
 }
