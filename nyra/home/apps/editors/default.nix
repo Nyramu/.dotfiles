2da90300d88bfe44f042e6409f43eedb7cb002a2
Nyramu <runamu05@gmail.com>
2025-12-04 19:24:59 +0100

    Added davinci-resolve and moved GIMP

--- nyra/home/apps/editors/default.nix
+++ nyra/home/apps/editors/default.nix
@@ -1,8 +1,21 @@
-{ ... }:
+{ config, lib, pkgs, ... }: with lib;
 
+let
+  cfg = config.nyra.home.apps.editors;
+in
 {
   imports = [
     ./nvf.nix
-    ./gimp.nix
   ];
+
+  options.nyra.home.apps.editors = {
+    davinci.enable = mkEnableOption "davinci-resolve";
+    gimp.enable = mkEnableOption "gimp";
+  };
+
+  config = {
+    home.packages = with pkgs;
+      optionals (cfg.davinci.enable) [ davinci-resolve ] ++
+      optionals (cfg.gimp.enable) [ gimp-with-plugins ];
+  };
 }
