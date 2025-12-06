8492c35c41f97c9b367b9feb39951af2ae8d1c3f
Nyramu <runamu05@gmail.com>
2025-12-06 23:38:00 +0100

    chore: removed unused themes

--- nyra/home/shells/zsh.nix
+++ nyra/home/shells/zsh.nix
@@ -2,29 +2,13 @@
 
 let
   # Fetch themes
-  yeknomhtooms = pkgs.fetchFromGitHub {
-    owner = "sebastianpulido";
-    repo = "oh-my-zsh";
-    rev = "master";
-    sha256 = "sha256-1gcdHxDNWAv8CeAWWE1CPzSZuVr59gn4fTHaQlHAm6o=";
-  };
-  jovial = pkgs.fetchFromGitHub {
-    owner = "zthxxx";
-    repo = "jovial";
-    rev = "master";
-    sha256 = "sha256-MxejCd58je1bIc0+IDn1brG1kxVWS0we2sT0wiPRr4c=";
-  };
   nyra = pkgs.fetchFromGitHub {
     owner = "Nyramu";
     repo = "nyra-zsh";
     rev = "main";
     sha256 = "sha256-90E//A3RaE2Z80rrxLLq/bEZm2K/oKShVDciaOq2N+A=";
   };
-  theme-path = {
-    yeknomhtooms = "${yeknomhtooms}/smoothmonkey.zsh-theme";
-    jovial = "${jovial}/jovial.zsh-theme";
-    nyra = "${nyra}/nyra.zsh-theme";
-  }.${cfg.theme};
+  theme-path = { nyra = "${nyra}/nyra.zsh-theme"; }.${cfg.theme};
 
   cfg = config.nyra.home.shells.zsh; 
 in
@@ -32,7 +16,7 @@ in
   options.nyra.home.shells.zsh = {
     enable = mkEnableOption "zsh";
     theme = mkOption {
-      type = types.enum [ "yeknomhtooms" "jovial" "nyra" ];
+      type = types.enum [ "nyra" ];
       default = "nyra";
       description = "choose zsh theme";
     };
