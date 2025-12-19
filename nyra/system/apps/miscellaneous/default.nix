aeebeb3147f9d63193998b769c0caa2c16ad5c98
Nyramu <107689027+Nyramu@users.noreply.github.com>
2025-12-19 16:18:37 +0100

    feat: remove packet tracer nyra option from nyra.system

--- nyra/system/apps/miscellaneous/default.nix
+++ nyra/system/apps/miscellaneous/default.nix
@@ -5,16 +5,4 @@ let
 in
 {
   imports = [];
-
-  options.nyra.system.apps.miscellaneous = {
-    ptracer.enable = mkEnableOption "packet tracer";
-  };
-
-  config = {
-    environment.systemPackages = with pkgs;
-      optionals (cfg.ptracer.enable) [ ciscoPacketTracer8 ];
-
-    nixpkgs.config.permittedInsecurePackages = with pkgs; 
-      optionals (cfg.ptracer.enable) [ ciscoPacketTracer8 ];
-  };
 }
