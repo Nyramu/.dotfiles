e7001913129ddfa23301b37ebf53e609ec0e7619
Nyramu <107689027+Nyramu@users.noreply.github.com>
2025-12-25 04:32:12 +0100

    chore: update helix config

--- nyra/home/apps/editors/helix.nix
+++ nyra/home/apps/editors/helix.nix
@@ -1,6 +1,10 @@
-{ config, lib, pkgs, ... }: with lib;
-
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
   cfg = config.nyra.home.apps.editors.helix;
 
   helix-wrapped = pkgs.writeShellScriptBin "hx" ''
@@ -39,6 +43,7 @@ in {
           bufferline = "multiple";
           color-modes = true;
           popup-border = "all";
+          continue-comments = false;
 
           cursor-shape = {
             normal = "block";
@@ -46,12 +51,20 @@ in {
             select = "underline";
           };
 
+          soft-wrap = {
+            enable = true;
+            max-wrap = 25;
+          };
+
           statusline = {
             left = ["mode" "spacer" "file-name" "read-only-indicator" "file-modification-indicator"];
             center = ["version-control"];
             right = ["spinner" "file-type" "diagnostics" "position" "position-percentage"];
+            mode.normal = "NORMAL";
+            mode.insert = "INSERT";
+            mode.select = "SELECT";
           };
-          
+
           lsp = {
             display-progress-messages = true;
             display-inlay-hints = true;
@@ -59,6 +72,7 @@ in {
 
           auto-save = {
             focus-lost = true;
+            after-delay.enable = true;
           };
 
           indent-guides = {
@@ -74,15 +88,65 @@ in {
         };
 
         keys.normal = {
-          C-q = ":wq";
-          C-s = ":w";
-          C-f = ":fmt";
-          C-w = ":buffer-close";
-          C-W = ":write-buffer-close";
-          C-right = ":buffer-next";
-          C-left = ":buffer-previous";
+          A-q = ":write-quit"; # Save and quit Helix
+          A-s = ":write"; # Save
+          A-f = ":format"; # Format code
+          A-esc = ":buffer-close!"; # Force close current file
+          A-ret = ":write-buffer-close"; # Save and close current file
+          A-right = ":buffer-next"; # Go to the next opened file
+          A-left = ":buffer-previous"; # Go to the previous opened file
+          A-up = "no_op";
+          A-down = "no_op";
 
-          space.g.g = ":sh zellij run -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- ${getExe pkgs.lazygit}";
+          C-z = "undo"; # Undo changes
+          C-y = "redo"; # Redo changes
+
+          space = {
+            g = ":sh zellij run -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- ${getExe pkgs.lazygit}";
+
+            s = "global_search";
+            f = "file_picker";
+            d = [":cd /home/nyramu/.dotfiles" "file_picker"]; # Go to .dotfiles
+            "/" = "no_op";
+          };
+        };
+
+        keys.insert = {
+          A-r = "replace_selections_with_clipboard"; # Replace selection with clipboard content
+          A-p = "paste_clipboard_before"; # Paste clipboard content before selection
+          A-y = "yank_to_clipboard"; # Yank selection to clipboard
+          A-d = "delete_selection"; # Delete selection
+          A-c = ["yank_to_clipboard" "delete_selection"]; # Cut selection
+
+          C-d = "kill_to_line_start"; # Delete till start of line
+          C-z = "undo"; # Undo changes
+          C-y = "redo"; # Redo changes
+          C-space = "toggle_comments"; # Comment/uncomment current line or selection
+
+          tab = "insert_tab";
+          S-ret = "insert_newline";
+
+          A-right = "move_next_word_end"; # Move to end of next word
+          A-left = "move_prev_word_start"; # Move to start of previous word
+          A-up = "move_visual_line_up"; # Move up
+          A-down = "move_visual_line_down"; # Move down
+
+          S-right = "extend_char_right"; # Extend to right
+          S-left = "extend_char_left"; # Extend to left
+          S-up = "extend_visual_line_up"; # Extend up
+          S-down = "extend_visual_line_down"; # Extend down
+
+          S-A-right = "extend_next_word_start"; # Extend to start of next word
+          S-A-left = "extend_prev_word_start"; # Extend to start of previous word
+          S-A-up = "extend_visual_line_up"; # Extend up
+          S-A-down = "extend_visual_line_down"; # Extend down
+        };
+
+        keys.select = {
+          S-right = "extend_next_word_start"; # Extend to start of next word
+          S-left = "extend_prev_word_start"; # Extend to start of previous word
+
+          C-space = "toggle_comments"; # Comment/uncomment current line or selection
         };
       };
 
@@ -93,10 +157,11 @@ in {
           clangd.command = "${pkgs.clang-tools}/bin/clangd";
           cmake-language-server.command = "${getExe pkgs.cmake-language-server}";
           csharp-ls.command = "${getExe pkgs.csharp-ls}";
-          #elixir-ls.command = "${getExe pkgs.elixir-ls}";
           #kotlin-language-server.command = "${getExe pkgs.kotlin-language-server}";
+          gopls.command = "${getExe pkgs.gopls}";
+          intelephense.command = "${getExe pkgs.intelephense}"; # PHP
+          superhtml.command = "${getExe pkgs.superhtml}";
           markdown-oxide.command = "${getExe pkgs.markdown-oxide}";
-          #rust-analyzer.command = "${getExe pkgs.rust-analyzer}";
           vscode-css-language-server.command = "${pkgs.vscode-langservers-extracted}/bin/vscode-css-language-server";
           typescript-language-server = {
             command = "${getExe pkgs.typescript-language-server}";
