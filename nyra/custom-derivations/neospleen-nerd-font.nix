6f72800eefb5314674831123a9fe695e34f0b6ee
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-10 15:18:50 +0100

    feat: add mono and sansSerif to  neospleen font derivation

--- nyra/custom-derivations/neospleen-nerd-font.nix
+++ nyra/custom-derivations/neospleen-nerd-font.nix
@@ -1,21 +1,54 @@
-{ lib, stdenvNoCC, fetchurl }:
-
+{ lib, stdenvNoCC, nerd-font-patcher }:
+  
 stdenvNoCC.mkDerivation rec {
   pname = "neospleen-nerd-font";
   version = "1.0.62";
 
-  src = fetchurl {
-    url = "https://github.com/mbwilding/NeoSpleen/releases/download/${version}/NeoSpleenNerdFont-Regular.ttf";
-    sha256 = "sha256-JYivzytGCmEcnu0Nv3AgfIhBFwIJzAcJFSudJ0Bj/VM=";
+  src = builtins.fetchurl {
+    url = "https://github.com/mbwilding/NeoSpleen/releases/download/${version}/NeoSpleen-Regular.ttf";
+    sha256 = "sha256:1ar71p1jqpgy3z0i1wzckicj64s7ilza70bmm0sgawlqgb55fhsc";
   };
+
+  nativeBuildInputs = [nerd-font-patcher];
 
   dontUnpack = true;
 
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p mono proportional
+
+    # Mono
+    cp $src mono/NeoSpleen-Regular.ttf
+    chmod +w mono/NeoSpleen-Regular.ttf
+    cd mono
+    nerd-font-patcher NeoSpleen-Regular.ttf \
+      --complete \
+      --careful \
+      --mono \
+      --outputdir .
+    cd ..
+
+    # Proportional
+    cp $src proportional/NeoSpleen-Regular.ttf
+    chmod +w proportional/NeoSpleen-Regular.ttf
+    cd proportional
+    nerd-font-patcher NeoSpleen-Regular.ttf \
+      --complete \
+      --careful \
+      --outputdir .
+    cd ..
+
+    runHook postBuild
+  '';
+
   installPhase = ''
     runHook preInstall
-    
-    install -Dm644 $src $out/share/fonts/truetype/NeoSpleenNerdFont-Regular.ttf
-    
+
+    mkdir -p $out/share/fonts/truetype
+    install -Dm644 mono/NeoSpleenNerdFontMono-Regular.ttf $out/share/fonts/truetype/
+    install -Dm644 proportional/NeoSpleenNerdFont-Regular.ttf $out/share/fonts/truetype/
+
     runHook postInstall
   '';
 
