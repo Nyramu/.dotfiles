f79c757fe4049d74aa9bb8a5d321035c24adce3c
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-12 20:44:43 +0100

    feat: add xdg-open config

--- profiles/base/home.nix
+++ profiles/base/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, userSettings, ... }:
+{ lib, pkgs, userSettings, ... }: with lib;
 
 let
   screenShot = pkgs.callPackage ../../nyra/commands/screen-shot {};
@@ -8,18 +8,18 @@ in
   imports = [
     ../../nyra/home
     ../../nyra/commands/nyra
-  ]; 
+  ];
 
   nixpkgs.config = {
     allowUnfree = true;
-    allowUnfreePredicate = (_ : true);
+    allowUnfreePredicate = _: true;
   };
 
   home = {
     username = userSettings.username;
     homeDirectory = "/home/${userSettings.username}";
     stateVersion = "26.05";
-    packages = [ screenShot screenRecord ];
+    packages = [screenShot screenRecord];
   };
   news.display = "silent";
 
@@ -27,4 +27,50 @@ in
 
   # Use zsh by default
   nyra.home.shells.zsh.enable = true;
+
+  # XDG configuration
+  xdg = {
+    enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = let
+        browser = ["zen-beta.desktop"];
+        imageViewer = ["imv.desktop"];
+        imageEditor = ["gimp.desktop"];
+        videoViewer = ["mpv.desktop"];
+
+        commonImages = ["image/png" "image/jpeg" "image/jpg" "image/gif" "image/webp" "image/bmp" "image/tiff" "image/svg+xml"];
+        gimpFormats = ["image/x-xcf" "image/x-psd" "image/x-xcfgz"];
+
+        videoFormats = ["video/mp4" "video/x-matroska" "video/webm" "video/mpeg" "video/x-msvideo" "video/quicktime" "video/x-flv" "video/ogg"];
+
+        toMimeAttrs = formats: opener:
+          formats
+          |> map (n: {
+            name = n;
+            value = opener;
+          })
+          |> builtins.listToAttrs;
+      in
+        mkMerge [
+          (toMimeAttrs commonImages imageViewer)
+          (toMimeAttrs gimpFormats imageEditor)
+          (toMimeAttrs videoFormats videoViewer)
+          {
+            "text/html" = browser;
+            "application/xhtml+xml" = browser;
+          }
+        ];
+    };
+    desktopEntries = {
+      imv = {
+        name = "imv";
+        exec = "${getExe pkgs.imv}";
+      };
+      mpv = {
+        name = "mpv";
+        exec = "${getExe pkgs.mpv} --keep-open=yes";
+      };
+    };
+  };
 }
