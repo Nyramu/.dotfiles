c3244233f07f03ddf6169f5173b476cd1fc4c4b7
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-16 00:28:54 +0100

    style(flake): reorder outputs and inputs for better accessibility

--- flake.nix
+++ flake.nix
@@ -1,57 +1,6 @@
 {
   description = "Nyramu's Personal Flake";
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
-    systemSettings = {
-      hostName = "nixos";
-      dotfiles = "~/.dotfiles";
-    };
-
-    userSettings = {
-      name = "Nyramu";
-      username = "nyramu";
-      email = "107689027+Nyramu@users.noreply.github.com";
-    };
-
-    inherit (nixpkgs) lib;
-
-    createNixosProfile = name: system:
-      lib.nixosSystem {
-        inherit system;
-        modules = [./profiles/${name}/configuration.nix];
-        specialArgs = {
-          inherit systemSettings userSettings inputs;
-        };
-      };
-
-    createHomeProfile = name: system:
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [];
-        };
-        modules = [./profiles/${name}/home.nix];
-        extraSpecialArgs = {
-          inherit systemSettings userSettings inputs;
-        };
-      };
-  in {
-    nixosConfigurations = {
-      main = createNixosProfile "main" "x86_64-linux";
-      potato = createNixosProfile "potato" "x86_64-linux";
-    };
-
-    homeConfigurations = {
-      main = createHomeProfile "main" "x86_64-linux";
-      potato = createHomeProfile "potato" "x86_64-linux";
-    };
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -104,6 +53,57 @@
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    systemSettings = {
+      hostName = "nixos";
+      dotfiles = "~/.dotfiles";
+    };
+
+    userSettings = {
+      name = "Nyramu";
+      username = "nyramu";
+      email = "107689027+Nyramu@users.noreply.github.com";
+    };
+
+    inherit (nixpkgs) lib;
+
+    createNixosProfile = name: system:
+      lib.nixosSystem {
+        inherit system;
+        modules = [./profiles/${name}/configuration.nix];
+        specialArgs = {
+          inherit systemSettings userSettings inputs;
+        };
+      };
+
+    createHomeProfile = name: system:
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+        modules = [./profiles/${name}/home.nix];
+        extraSpecialArgs = {
+          inherit systemSettings userSettings inputs;
+        };
+      };
+  in {
+    nixosConfigurations = {
+      main = createNixosProfile "main" "x86_64-linux";
+      potato = createNixosProfile "potato" "x86_64-linux";
+    };
+
+    homeConfigurations = {
+      main = createHomeProfile "main" "x86_64-linux";
+      potato = createHomeProfile "potato" "x86_64-linux";
     };
   };
 }
