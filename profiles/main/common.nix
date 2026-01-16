b0872cbb594c6af8d1e2d6276e3ae59c5cc70cc7
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-17 00:09:22 +0100

    chore(config): change theme

--- profiles/main/common.nix
+++ profiles/main/common.nix
@@ -4,6 +4,6 @@
   # Theming
   nyra.theme = {
     enable = true;
-    defaultTheme = "just-chill"; # just-chill | red-eclipse | apocalypse
+    defaultTheme = "apocalypse"; # just-chill | red-eclipse | apocalypse
   };
 }
