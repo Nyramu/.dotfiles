c1f0bf207ccc5e50a6d7fd2333faa86c73387c53
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-17 02:27:04 +0100

    chore(theme): update template.nix

--- resources/themes/template.nix
+++ resources/themes/template.nix
@@ -1,11 +1,11 @@
 { pkgs }:
 
-rec
+rec # Needed to "cross-link" variables (ugh.)
 {
   system = "base16";
   name = "Theme Name";
   author = "https://github.com/Nyramu";
-  polarity = "dark";
+  polarity = "dark"; # Or "light", but we don't use flashbangs here
 
   background = /path/to/background.jpg; # Or a fetchurl
     
@@ -35,67 +35,59 @@ rec
   }; 
 
   opacity = {
-    terminal = 0.63;
-    applications = 0.6;
-    desktop = 0.7;
-    popups = 0.7;
+    terminal = 0.63; # Default: 0.63
+    applications = 0.6; # Default: 0.6
+    desktop = 0.7; # Default: 0.7
+    popups = 0.7; # Default: 0.7
   };
 
   palette = {
-    base00 = "#eeeeee"; # Default Background -
-    base01 = "#eeeeee"; # Lighter Background - 
-    base02 = "#eeeeee"; # Selection Background - 
-    base03 = "#eeeeee"; # Comments, Invisibles - 
-    base04 = "#eeeeee"; # Dark Foreground - 
-    base05 = "#eeeeee"; # Default Foreground - 
-    base06 = "#eeeeee"; # Light Foreground - 
-    base07 = "#eeeeee"; # Light Background - 
+    base00 = "#eeeeee"; # Default Background - color and tonality
+    base01 = "#eeeeee"; # Lighter Background - color and tonality
+    base02 = "#eeeeee"; # Selection Background - color and tonality
+    base03 = "#eeeeee"; # Comments, Invisibles - color and tonality
+    base04 = "#eeeeee"; # Dark Foreground - color and tonality
+    base05 = "#eeeeee"; # Default Foreground - color and tonality
+    base06 = "#eeeeee"; # Light Foreground - color and tonality
+    base07 = "#eeeeee"; # Light Background - color and tonality
 
-    base08 = "#eeeeee"; # Variables, Deleted - 
-    base09 = "#eeeeee"; # Integers, Constants - 
-    base0A = "#eeeeee"; # Classes, Bold - 
-    base0B = "#eeeeee"; # Strings, Inserted - 
-    base0C = "#eeeeee"; # Support, Regex - 
-    base0D = "#eeeeee"; # Functions, Headings - 
-    base0E = "#eeeeee"; # Keywords, Italic - 
-    base0F = "#eeeeee"; # Deprecated - 
+    base08 = "#eeeeee"; # Variables, Deleted - color and tonality
+    base09 = "#eeeeee"; # Integers, Constants - color and tonality
+    base0A = "#eeeeee"; # Classes, Bold - color and tonality
+    base0B = "#eeeeee"; # Strings, Inserted - color and tonality
+    base0C = "#eeeeee"; # Support, Regex - color and tonality
+    base0D = "#eeeeee"; # Functions, Headings - color and tonality
+    base0E = "#eeeeee"; # Keywords, Italic - color and tonality
+    base0F = "#eeeeee"; # Deprecated - color and tonality
   };
 
   # Hyprland color configuration
   hypr = {
     active_border_color = "rgb(eeeeee) rgb(eeeeee) rgb(eeeeee) 270deg";
     inactive_border_color = "rgb(eeeeee)";
-    locked_group_active_border_color = "rgb(eeeeee) rgb(eeeeee) rgb(eeeeee) 270deg"; # Picked from wallpaper
+    locked_group_active_border_color = "rgb(eeeeee) rgb(eeeeee) rgb(eeeeee) 270deg"; # Tip: Pick from wallpaper
     
     text_color = "rgb(eeeeee)";
     inactive_text_color = "rgb(eeeeee)";
-    locked_group_active_text_color = "rgb(eeeeee)"; # Picked from wallpaper, lighter than locked active border
+    locked_group_active_text_color = "rgb(eeeeee)"; # Tip: Lighter than locked active border
 
-    active_color = "rgb(eeeeee)"; # Last color from active border
-    inactive_color = "rgb(eeeeee)"; # Color from inactive border
-    locked_group_active_color = "rgb(eeeeee)"; # Last color from locked active border
-  };
-
-  # Waybar color configuration
-  waybar = {
-    glyph-color = "#eeeeee";
-    background-color = "rgba(38, 38, 38, 0.6)";
-    font = fonts.monospace.name;
-    font-size = "15";
+    active_color = "rgb(eeeeee)"; # Tip: Last color from active border
+    inactive_color = "rgb(eeeeee)"; # Tip: Color from inactive border
+    locked_group_active_color = "rgb(eeeeee)"; # Tip: Last color from locked active border
   };
   
   # Vicinae configuration
   vicinae = {
     font = {
       normal = {
-        size = 10.5;
-        normal = fonts.monospace.name;
+        size = 10.5; # It's generally better between 10.5 and 12
+        normal = fonts.monospace.name; # monospace | sansSerif | serif
       };
     };
   };
 
   # SilentSDDM configuration
   silentSDDM = {
-    avatar-shape = "square"; # Or "circle"
+    avatar-shape = "square"; # "square" | "circle"
   };
 }
