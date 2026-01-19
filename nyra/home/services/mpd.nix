60d6a81202f62b891e67b7a52fcd3ac3dae53bb5
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-20 00:08:17 +0100

    rework(nyra): move mpd to nyra.home.services

---
+++ nyra/home/services/mpd.nix
@@ -0,0 +1,59 @@
+{ config, lib, ... }:
+
+{
+  options.nyra.home.services.mpd = {
+    enable = lib.mkEnableOption "mpd";
+  };
+
+  config = {
+    services.mpd = {
+      enable = config.nyra.home.services.mpd.enable;
+      musicDirectory = "${config.home.homeDirectory}/Music";
+      playlistDirectory = "${config.home.homeDirectory}/Music/Playlists";
+      extraConfig = ''
+        audio_output {
+          type       "pipewire"
+          name       "PipeWire"
+          mixer_type "software"
+        }
+
+        audio_output {
+          type    "fifo"
+          name    "Visualizer"
+          path    "/tmp/mpd.fifo"
+          format  "44100:16:2"
+        }
+
+        auto_update "yes"
+      '';
+      # TODO: make possible to use common nyra options to allow synchronization between home and system
+    #   extraConfig = if (config.nyra.system.hardware.audio == "pipewire") then ''
+    #     audio_output {
+    #       type       "pipewire"
+    #       name       "PipeWire"
+    #       mixer_type "software"
+    #     }
+
+    #     audio_output {
+    #       type    "fifo"
+    #       name    "Visualizer"
+    #       path    "/tmp/mpd.fifo"
+    #       format  "44100:16:2"
+    #     }
+
+    #     auto_update "yes"
+    #   ''
+    #   else ''
+    #     audio_output {
+    #       type       "pulse"
+    #       name       "PulseAudio"
+    #       mixer_type "software"
+    #     }
+    #   '';
+    };
+    services.mpd-mpris.enable = config.services.mpd.enable;
+    programs.cava = {
+      enable = config.nyra.home.apps.music.enable;
+    };
+  };
+}
