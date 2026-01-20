15d46005e58de2f94e23371a556cd37ae8b1121c
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-21 00:20:42 +0100

    rework(nyra): merge nyra.system.fonts into nyra system default

--- nyra/system/default.nix
+++ nyra/system/default.nix
@@ -1,14 +1,35 @@
-{ inputs, ... }:
+{ config, lib, inputs, pkgs, ... }:
 
+let
+  theme = import ../../resources/themes/${config.nyra.theme.defaultTheme}.nix {inherit pkgs;};
+in
 {
   imports = [
     inputs.stylix.nixosModules.stylix
     ./apps
     ./hardware
     ./desktops
-    ./fonts
     ./login
     ./security
     ../common
   ];
+
+  options.nyra.system = {
+    fonts = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [];
+    };
+  };
+  
+  # "fc-list : family | sort | uniq" to list
+  config = {
+    nyra.system.fonts = lib.mkIf (config.nyra.theme.enable) [
+      theme.fonts.serif.package
+      theme.fonts.sansSerif.package
+      theme.fonts.monospace.package
+      theme.fonts.emoji.package
+    ];
+    fonts.packages = config.nyra.system.fonts;
+    fonts.enableDefaultPackages = true;
+  };
 }
