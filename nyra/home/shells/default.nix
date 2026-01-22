c72f73254466331bdf2019f8ef7ae40a06131c91
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-23 00:56:00 +0100

    feat(shells): add oh-my-posh

--- nyra/home/shells/default.nix
+++ nyra/home/shells/default.nix
@@ -3,6 +3,17 @@
 {
   imports = [
     ./zsh.nix
+    ./nushell.nix
     ./commands
   ];
+
+  programs.oh-my-posh = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableNushellIntegration = true;
+    settings = {
+      
+    };
+  };
 }
