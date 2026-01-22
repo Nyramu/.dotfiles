7a2c9e77279266d67a4a1f7fa46cb5af7fcc7a76
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-22 01:32:46 +0100

    fix(base): typo in userSettings implementation in configuration.nix

--- profiles/base/configuration.nix
+++ profiles/base/configuration.nix
@@ -18,8 +18,8 @@
   networking.hostName = systemSettings.hostName;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  nix.settings.trusted-users = [ userSettings.userName ];
-  users.users.${userSettings.userName} = {
+  nix.settings.trusted-users = [ userSettings.username ];
+  users.users.${userSettings.username} = {
     isNormalUser = true;
     description = "${userSettings.name}";
     extraGroups = [
