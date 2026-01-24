4317351939513680956bbe4472fed25c506e6225
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-01-24 19:45:20 +0100

    chore(zen): update mods config

--- nyra/home/apps/browsers/zen/mods.nix
+++ nyra/home/apps/browsers/zen/mods.nix
@@ -1,8 +1,11 @@
 { config, ... }:
 
+let
+  nyraSettings = config.nyra.settings;
+in
 {
   programs.zen-browser = {
-    profiles.${config.nyra.settings.username}.mods = [
+    profiles.${nyraSettings.username}.mods = [
       "f4866f39-cfd6-4498-ab92-54213b8279dc" # Animations Plus
       "ad97bb70-0066-4e42-9b5f-173a5e42c6fc" # SuperPins
       "cb5efa80-f1e1-43ce-8c0b-fece8462d225" # Container Halo
