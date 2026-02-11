5b7d59a98071735be72ca5847cd0755c2d415115
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-02-11 22:50:46 +0100

    chore(spotiflac): update derivation version

--- nyra/custom-derivations/spotiflac.nix
+++ nyra/custom-derivations/spotiflac.nix
@@ -6,7 +6,7 @@
 }:
 let
   pname = "spotiflac";
-  version = "7.0.8";
+  version = "7.0.9";
 
   src = fetchurl {
     url = "https://github.com/afkarxyz/SpotiFLAC/releases/download/v${version}/SpotiFLAC.AppImage";
