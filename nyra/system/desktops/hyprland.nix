bea9d52451971ac7a515d09afd159cfb83c0adf6
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-03-12 14:20:19 +0100

    chore(hyprland): update package on system side

--- nyra/system/desktops/hyprland.nix
+++ nyra/system/desktops/hyprland.nix
@@ -1,8 +1,14 @@
-{ config, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 
 {
   programs.hyprland = {
     enable = config.nyra.desktops.hyprland.enable;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     xwayland.enable = true;
   };
 }
