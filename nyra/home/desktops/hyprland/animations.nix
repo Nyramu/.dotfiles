966b0f73bec049974600abcd073c89da5251700d
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-03-28 19:35:11 +0100

    chore(hyprland): update animations

--- nyra/home/desktops/hyprland/animations.nix
+++ nyra/home/desktops/hyprland/animations.nix
@@ -1,60 +1,63 @@
-{ ... }:
-
 {
   wayland.windowManager.hyprland = {
-    animations.animation = {
-      # Window creation
-      windowsIn = {
-        enable = true;
-        duration = 200;
-        curve = "easeOutCirc";
-        style = "gnomed";
+    animations = {
+      bezierCurve = {
+        holo = [
+          0.23
+          1
+          0.32
+          1
+        ];
+        data = [
+          0.16
+          1
+          0.3
+          1
+        ];
       };
-      # Window destruction
-      windowsOut = {
-        enable = true;
-        duration = 300;
-        curve = "easeOutCirc";
-        style = "gnomed";
-      };
-      # Window movement
-      windowsMove = {
-        enable = true;
-        duration = 300;
-        curve = "easeInOutCubic";
-        style = "gnomed";
-      };
-
-      # Idk what it does, maybe it's associated with windowsIn
-      fadeIn = {
-        enable = true;
-        duration = 300;
-        curve = "easeOutCirc";
-      };
-      # Idk what it does, maybe it's associated with windowsOut
-      fadeOut = {
-        enable = true;
-        duration = 300;
-        curve = "easeOutCirc";
-      };
-
-      # Border animation speed
-      border = {
-        enable = true;
-        duration = 100;
-      };
-      # Border animation fluidity
-      borderangle = {
-        enable = true;
-        duration = 3000;
-      };
-
-      # Workspace switch
-      workspaces = {
-        enable = true;
-        duration = 400;
-        curve = "easeOutExpo";
-        style = "fade";
+      
+      animation = {
+        windowsIn = {
+          enable = true;
+          duration = 500;
+          curve = "holo";
+          style = "slide";
+        };
+        windowsOut = {
+          enable = true;
+          duration = 400;
+          curve = "holo";
+          style = "popin 100%";
+        };
+        windowsMove = {
+          enable = true;
+          duration = 500;
+          curve = "holo";
+          style = "slide";
+        };
+        fade = {
+          enable = true;
+          duration = 500;
+          curve = "data";
+        };
+        layers = {
+          enable = true;
+          duration = 500;
+          curve = "holo";
+          style = "fade";
+        };
+        workspaces = {
+          enable = true;
+          duration = 600;
+          curve = "holo";
+          style = "slidevert";
+        };
+        specialWorkspace = {
+          enable = true;
+          duration = 600;
+          curve = "holo";
+          style = "slidevert";
+        };
       };
     };
   };
