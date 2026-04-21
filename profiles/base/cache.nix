99b7dd2fec456bf00763a9ded1efed32d06c8730
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-04-21 12:16:58 +0200

    chore(cache): update caches

--- profiles/base/cache.nix
+++ profiles/base/cache.nix
@@ -9,6 +9,8 @@
       "https://hyprland.cachix.org"
       # Eden Emulator
       "https://eden-emu-flake.cachix.org"
+      # CachyOS Kernels
+      "https://attic.xuyh0120.win/lantian"
     ];
     trusted-substituters = substituters;
     trusted-public-keys = [
@@ -18,6 +20,8 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       # Eden Emulator
       "eden-emu-flake.cachix.org-1:qMKfHpBsRhMpxrc4FawVy+IeYAvFhhv94qiB69Bl0RU="
+      # CachyOS Kernels
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
     ];
   };
 }
