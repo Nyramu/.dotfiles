c6996c4d1b5f862818290f7dbc3cc8376c460bda
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-10 20:56:32 +0200

    refactor(dendritic): port httpd

---
+++ modules/nyra/services/httpd.nix
@@ -0,0 +1,44 @@
+{ self, lib, ... }:
+{
+  flake.modules.nixos = {
+    services.imports = [ self.modules.nixos.httpd ];
+
+    httpd =
+      { config, user, ... }:
+
+      let
+        cfg = config.nyra.services.httpd;
+        dir = "/home/${user.name}/localhost";
+      in
+      {
+        options.nyra.services.httpd = {
+          enable = lib.mkEnableOption "httpd";
+        };
+
+        config = lib.mkIf (cfg.enable) {
+          services.httpd = {
+            enable = true;
+            enablePHP = true;
+            adminAddr = "admin@localhost";
+            user = "${user.name}";
+            group = "users";
+            extraModules = [ "rewrite" ];
+
+            virtualHosts."localhost" = {
+              documentRoot = "${dir}";
+              extraConfig = ''
+                <Directory "${dir}">
+                  Options Indexes FollowSymLinks
+                  AllowOverride All
+                  Require all granted
+                </Directory>
+                DirectoryIndex index.php index.html
+              '';
+            };
+          };
+
+          networking.firewall.allowedTCPPorts = [ 80 ];
+        };
+      };
+  };
+}
