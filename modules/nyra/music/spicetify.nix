ea64bd3e1e6bc37969b0d09341a8118228d953fc
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-10 17:32:40 +0200

    refactor(dendritic): port spicetify
    nyra.home.apps.misc.spicetify -> nyra.music.spicetify

---
+++ modules/nyra/music/spicetify.nix
@@ -0,0 +1,56 @@
+{
+  self,
+  inputs,
+  lib,
+}:
+{
+  flake.modules.homeManager = {
+    music.imports = [ self.modules.homeManager.spicetify ];
+
+    spicetify =
+      {
+        config,
+        wayland,
+        pkgs,
+        ...
+      }:
+
+      let
+        cfg = config.nyra.music.spicetify;
+        spicePkgs = inputs.spicetify.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+      in
+      {
+        imports = [ inputs.spicetify.homeManagerModules.spicetify ];
+
+        options.nyra.music.spicetify = {
+          enable = lib.mkEnableOption "spicetify";
+        };
+
+        config = lib.mkIf (cfg.enable) {
+          programs.spicetify = {
+            enable = true;
+            wayland = wayland.enable;
+            windowManagerPatch = true;
+            enabledCustomApps = with spicePkgs.apps; [
+              newReleases
+              marketplace
+              ncsVisualizer
+            ];
+            enabledExtensions = with spicePkgs.extensions; [
+              autoSkipVideo
+              autoVolume
+              adblockify
+              fullAppDisplay
+              hidePodcasts
+              showQueueDuration
+              betterGenres
+              savePlaylists
+              volumePercentage
+              beautifulLyrics
+              aiBandBlocker
+            ];
+          };
+        };
+      };
+  };
+}
