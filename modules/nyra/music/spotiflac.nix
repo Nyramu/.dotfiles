f332be0492ab49f615f531ea582e7d1528c0b274
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-10 17:33:15 +0200

    refactor(dendritic): port spotiflac
    nyra.home.apps.misc.spotiflac -> nyra.music.spotiflac

---
+++ modules/nyra/music/spotiflac.nix
@@ -0,0 +1,28 @@
+{ self, lib, ... }:
+{
+  flake.modules.homeManager = {
+    music.imports = [ self.modules.homeManager.spotiflac ];
+
+    spotiflac =
+      {
+        config,
+        host,
+        ...
+      }:
+
+      let
+        cfg = config.nyra.music.spotiflac;
+      in
+      {
+        options.nyra.music.spotiflac = {
+          enable = lib.mkEnableOption "spotiflac";
+        };
+
+        config = lib.mkIf (cfg.enable) {
+          home.packages = [
+            config.flake.packages.${host.system}.spotiflac
+          ];
+        };
+      };
+  };
+}
