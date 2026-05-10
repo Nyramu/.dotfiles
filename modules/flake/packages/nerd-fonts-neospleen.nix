90fd7c076fa7cb1518684fad790474c67f6c5fe9
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-10 17:30:06 +0200

    refactor(dendritic): port neospleen nerd font derivation

---
+++ modules/flake/packages/nerd-fonts-neospleen.nix
@@ -0,0 +1,65 @@
+{
+  perSystem =
+    { pkgs, lib, ... }:
+    {
+      packages.nerd-fonts-neospleen = pkgs.stdenvNoCC.mkDerivation rec {
+        pname = "nerd-fonts-neospleen";
+        version = "1.1.63";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/mbwilding/NeoSpleen/releases/download/${version}/NeoSpleen-Regular.ttf";
+          hash = "sha256-uhOThDRNYIkxwJ+4bsRavrKKBL+VK+wuqlAxkvFg6JQ=";
+        };
+
+        nativeBuildInputs = with pkgs; [ nerd-font-patcher ];
+
+        dontUnpack = true;
+
+        buildPhase = ''
+          runHook preBuild
+
+          mkdir -p mono proportional
+
+          # Mono
+          cp $src mono/NeoSpleen-Regular.ttf
+          chmod +w mono/NeoSpleen-Regular.ttf
+          cd mono
+          nerd-font-patcher NeoSpleen-Regular.ttf \
+            --complete \
+            --careful \
+            --mono \
+            --outputdir .
+          cd ..
+
+          # Proportional
+          cp $src proportional/NeoSpleen-Regular.ttf
+          chmod +w proportional/NeoSpleen-Regular.ttf
+          cd proportional
+          nerd-font-patcher NeoSpleen-Regular.ttf \
+            --complete \
+            --careful \
+            --outputdir .
+          cd ..
+
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/share/fonts/truetype
+          install -Dm644 mono/NeoSpleenNerdFontMono-Regular.ttf $out/share/fonts/truetype/
+          install -Dm644 proportional/NeoSpleenNerdFont-Regular.ttf $out/share/fonts/truetype/
+
+          runHook postInstall
+        '';
+
+        meta = with lib; {
+          description = "NeoSpleen Nerd Font - Spleen bitmap font with Nerd Font patches";
+          homepage = "https://github.com/mbwilding/NeoSpleen";
+          license = licenses.bsd2;
+          platforms = platforms.all;
+        };
+      };
+    };
+}
