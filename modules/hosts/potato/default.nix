a129fda54522806189fbfbeff507d2ecfd9ffa15
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-12 01:12:05 +0200

    chore(potato): update config

--- modules/hosts/potato/default.nix
+++ modules/hosts/potato/default.nix
@@ -49,10 +49,12 @@ in
 
         nyra.desktops.hyprland = {
           enable = true;
-          dynamic-cursors.enable = true;
           shader = {
             enable = true;
             name = "less-saturation";
+          };
+          plugins = {
+            dynamic-cursors.enable = true;
           };
           monitors = [
             {
