3daa37795a3df59a17f7d911f8a0960a08620a62
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-11 21:32:28 +0200

    fix(vicinae): remove duplicated imports

--- modules/nyra/desktops/layers/vicinae/extensions.nix
+++ modules/nyra/desktops/layers/vicinae/extensions.nix
@@ -1,24 +1,20 @@
-{ self, inputs, ... }:
+{ inputs, ... }:
 {
-  flake.modules.homeManager = {
-    desktops.layers.imports = [ self.modules.homeManager.vicinae ];
-
-    vicinae =
-      { pkgs, ... }:
-      {
-        services.vicinae = {
-          extensions =
-            with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system};
-            with inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system};
-            [
-              hypr-keybinds
-              color-converter
-              process-manager
-              nerdfont-search
-              noctalia-shell-wallpaper-selector
-              protondb-search
-            ];
-        };
+  flake.modules.homeManager.vicinae =
+    { pkgs, ... }:
+    {
+      services.vicinae = {
+        extensions =
+          with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system};
+          with inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system};
+          [
+            hypr-keybinds
+            color-converter
+            process-manager
+            nerdfont-search
+            noctalia-shell-wallpaper-selector
+            protondb-search
+          ];
       };
-  };
+    };
 }
