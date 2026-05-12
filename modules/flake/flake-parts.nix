8f633f4c53269641763949e275685eb60599b0d3
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-13 01:21:12 +0200

    feat(flake-parts): add key in every module

--- modules/flake/flake-parts.nix
+++ modules/flake/flake-parts.nix
@@ -1,5 +1,30 @@
-{ inputs, ... }:
+{ lib, moduleLocation, ... }:
 
+let
+  inherit (lib)
+    mapAttrs
+    mkOption
+    types
+    ;
+
+  escapeNixIdentifier =
+    s: if builtins.match "[a-zA-Z_][a-zA-Z0-9_'\\-]*" s != null then s else lib.escapeNixString s;
+
+  wrapModule = class: moduleName: module: {
+    key = "${toString moduleLocation}#modules.${escapeNixIdentifier class}.${escapeNixIdentifier moduleName}";
+    _file = "${toString moduleLocation}#modules.${escapeNixIdentifier class}.${escapeNixIdentifier moduleName}";
+    _class = class;
+    imports = [ module ];
+  };
+in
 {
-  imports = [ inputs.flake-parts.flakeModules.modules ];
+  # imports = [ inputs.flake-parts.flakeModules.modules ];
+  options.flake.modules = mkOption {
+    type = types.lazyAttrsOf (types.lazyAttrsOf types.deferredModule);
+    default = { };
+    apply = mapAttrs (class: mapAttrs (wrapModule class));
+    description = ''
+      Groups of modules published by the flake, with automatic `key` for deduplication.
+    '';
+  };
 }
