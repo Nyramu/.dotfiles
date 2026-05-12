fc1fbb6c2ab0f27debbf6b5109bc65d00ba8b62d
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-13 00:46:14 +0200

    refactor(dendritic): update install.sh

--- install.sh
+++ install.sh
@@ -46,28 +46,27 @@ else
   exit 1
 fi
 
-print_header "Profile selection"
+print_header "Host selection"
 echo ""
 
-# Load profiles
-declare -A profiles
-for profile in profiles/*; do
-  profile=$(basename "$profile")
-  if [ "$profile" = "base" ]; then continue; fi
-  profiles["$profile"]="$profile"
+# Load hosts
+declare -A hosts
+for host in modules/hosts/*; do
+  host=$(basename "$host")
+  hosts["$host"]="$host"
 done
 
-echo "Select profile:"
-for profile in "${profiles[@]}"; do
-  echo -e "  - ${GREEN}${profile}${NC}"
+echo "Select host:"
+for host in "${hosts[@]}"; do
+  echo -e "  - ${GREEN}${host}${NC}"
 done
 
 echo ""
 printf "Selection: "
-read -r profile
+read -r host
 
-if [[ ! -v profiles["$profile"] ]]; then
-  print_error "profile ${profile} does not exist, aborting"
+if [[ ! -v hosts["$host"] ]]; then
+  print_error "host ${host} does not exist, aborting"
   exit 1
 fi
 
@@ -77,35 +76,26 @@ printf "[y/N]: "
 read -r generate
 
 if [[ $generate = "y" ]]; then
-  sudo nixos-generate-config --show-hardware-config > profiles/"${profile}"/hardware-configuration.nix
+  sudo nixos-generate-config --show-hardware-config > modules/hosts/"${host}"/_hardware-configuration.nix
 fi
 
 echo ""
-print_header "Profile installation"
+print_header "Host installation"
 echo ""
-print_info "Installing $profile profile"
-echo ""
-
-sudo nixos-rebuild switch --option extra-experimental-features "nix-command flakes pipe-operators" --flake ~/.dotfiles#"$profile"
-
-echo ""
-print_success "System part of the $profile profile has been installed"
+print_info "Installing $host host"
 echo ""
 
-nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.dotfiles#"$profile"
+sudo nixos-rebuild switch --option extra-experimental-features "nix-command flakes pipe-operators" --flake ~/.dotfiles#"$host"
 
 echo ""
-print_success "Home-manager part of the $profile profile has been installed"
+print_success "System part of the $host host has been installed"
 echo ""
 
-mkdir -p ~/Music/Lyrics/
-mkdir -p ~/Music/Playlists/
-mkdir -p ~/Desktop/
-mkdir -p ~/Downloads/
-mkdir -p ~/Documents/  
-mkdir -p ~/Videos/Recordings/
-mkdir -p ~/Pictures/Screenshots/
-mkdir -p ~/Games/
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.dotfiles#"$host"
+
+echo ""
+print_success "Home-manager part of the $host host has been installed"
+echo ""
 
 print_success "Home user directories have been created"
 echo ""
