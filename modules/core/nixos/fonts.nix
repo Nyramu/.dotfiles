1bb9dac1e1cb6248d9bbbdbd55887d7a95071421
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-05-12 12:58:30 +0200

    chore(core/fonts): add comments to help fonts usage

--- modules/core/nixos/fonts.nix
+++ modules/core/nixos/fonts.nix
@@ -6,6 +6,8 @@
     fonts =
       { pkgs, ... }:
       {
+        # "fc-cache -fv" to manually refresh cache
+        # "fc-list : family | sort | uniq" to list
         fonts = {
           packages = with pkgs; [
             nerd-fonts.dejavu-sans-mono
