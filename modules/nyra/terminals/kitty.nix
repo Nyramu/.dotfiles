38f064176f8971bf56d70d8ea0354556840b502e
Nyramu <nyramu@noreply.codeberg.org>
2026-05-17 23:28:49 +0200

    chore(kitty): remove dependency from Stylix

--- modules/nyra/terminals/kitty.nix
+++ modules/nyra/terminals/kitty.nix
@@ -9,7 +9,6 @@
       let
         cfg = config.nyra.terminals.kitty;
         default = config.nyra.terminals.default;
-        palette = config.stylix.base16Scheme;
       in
       {
         options.nyra.terminals.kitty = {
@@ -28,8 +27,6 @@
             settings = {
               shell_integration = "enabled";
               confirm_os_window_close = 0;
-              # Does not work for some reason
-              #startup_session = /home/nyramu/.dotfiles;
               allow_remote_control = "no";
               listen_on = "none";
               sync_to_monitor = "yes";
@@ -39,20 +36,11 @@
               scrollbar = "scrolled";
               scrollbar_interactive = "yes";
               scrollbar_jump_on_click = "yes";
-              scrollbar_handle_color = lib.mkDefault palette.base0A;
               background_blur = 0;
               cursor_trail = 60;
-              cursor_trail_color = lib.mkDefault palette.base0A;
               cursor_shape_unfocused = "unchanged";
-
               window_padding_width = "0 8";
             };
-            # Fix for Stylix overriding normal settings
-            extraConfig = ''
-              selection_background ${palette.base02}
-              selection_foreground none
-              color8 ${palette.base03} 
-            '';
           };
           nyra.terminals.kitty.enable = lib.mkDefault (default == "kitty");
         };
