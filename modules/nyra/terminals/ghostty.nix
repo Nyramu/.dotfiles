a581f8ad8dfd2b6ca8b1691051ae756dac13ccba
Nyramu <nyramu@noreply.codeberg.org>
2026-05-25 14:39:44 +0200

    chore(ghostty): update config

--- modules/nyra/terminals/ghostty.nix
+++ modules/nyra/terminals/ghostty.nix
@@ -23,12 +23,25 @@
             enableFishIntegration = (shell == "fish");
             systemd.enable = true;
             settings = {
+              selection-clear-on-copy = false;
+              cursor-style = "bar";
+              cursor-style-blink = true;
+              cursor-click-to-move = true;
+              mouse-hide-while-typing = true;
+              notify-on-command-finish = "unfocused";
+              notify-on-command-finish-action = "no-bell,notify";
               working-directory = "inherit";
+              window-inherit-working-directory = true;
+              tab-inherit-working-directory = false;
+              split-inherit-working-directory = true;
               confirm-close-surface = false;
               right-click-action = "copy-or-paste";
               clipboard-read = "allow";
               clipboard-write = "allow";
-              clipboard-paste-protection = false;
+              clipboard-paste-protection = true;
+              auto-update = "off";
+              window-padding-x = 12;
+              window-padding-y = 8;
             };
           };
           nyra.terminals.ghostty.enable = lib.mkDefault (default == "ghostty");
