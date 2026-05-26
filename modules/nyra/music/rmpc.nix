b0afd65bc384fe535260a6be08a7f887b7d3c0e9
Nyramu <nyramu@noreply.codeberg.org>
2026-05-26 23:17:49 +0200

    chore(rmpc): lower rewind_to_start_sec to 10

--- modules/nyra/music/rmpc.nix
+++ modules/nyra/music/rmpc.nix
@@ -26,7 +26,7 @@
               #![enable(unwrap_variant_newtypes)]
               (
                 lyrics_dir: Some("${dirs.extraConfig.LYRICS}"),
-                rewind_to_start_sec: Some(90),
+                rewind_to_start_sec: Some(10),
                 password: None,
                 theme: Some("theme.ron"),
                 cache_dir: None,
