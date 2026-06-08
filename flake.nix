b5f28448859a50b37a0c6090d48047ef14e7a513
Nyramu <nyramu@noreply.codeberg.org>
2026-06-09 01:33:02 +0200

    chore(flake): add noctalia v5 input

--- flake.nix
+++ flake.nix
@@ -19,8 +19,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+    noctalia.url = "github:noctalia-dev/noctalia-shell";
+    noctalia-shell = {
+      url = "github:noctalia-dev/noctalia-shell/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
