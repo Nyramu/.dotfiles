25c9ee94364146cfc7259b2b8005e54fbfc42266
Nyramu <nyramu@noreply.codeberg.org>
2026-06-14 20:10:39 +0200

    chore(themes/red-eclipse): update config

--- modules/themes/red-eclipse/default.nix
+++ modules/themes/red-eclipse/default.nix
@@ -17,35 +17,63 @@
           hyprnix.settings = lib.mkIf (cfg.desktops.hyprland.enable) rec {
             general = {
               col = {
-                active_border = "rgb(1a1625) rgb(b83858) rgb(983050) 330deg";
-                inactive_border = "rgb(1a1625)";
+                active_border = {
+                  colors = [
+                    "rgb(1a1625)"
+                    "rgb(b83858)"
+                    "rgb(983050)"
+                  ];
+                  angle = 330;
+                };
+                inactive_border = {
+                  colors = [
+                    "rgb(1a1625)"
+                  ];
+                };
               };
             };
-
             group = {
               col = {
                 border_active = general.col.active_border;
                 border_inactive = general.col.inactive_border;
-                # Picked from wallpaper
-                border_locked_active = "rgb(1a1625) rgb(5f1d25) rgb(ccb0ad) 270deg";
+                border_locked_active = {
+                  colors = [
+                    "rgb(1a1625)"
+                    "rgb(5f1d25)"
+                    "rgb(ccb0ad)"
+                  ];
+                  angle = 270;
+                };
                 border_locked_inactive = general.col.inactive_border;
               };
               groupbar = {
-                text_color = "rgb(faeaf0)"; # palette.base07
-                text_color_inactive = "rgb(5a4560)"; # palette.base04
-                text_color_locked_active = "rgb(faeaf0)"; # Picked from wallpaper, lighter than locked active border
+                text_color = "rgb(faeaf0)";
+                text_color_inactive = "rgb(5a4560)";
+                text_color_locked_active = "rgb(faeaf0)";
                 text_color_locked_inactive = group.groupbar.text_color_inactive;
                 col = {
-                  active = "rgb(983050)"; # Last color from active border
-                  inactive = "rgb(1a1625)"; # Color from inactive border
-                  locked_active = "rgb(ccb0ad)"; # Last color from locked active border
+                  active = {
+                    colors = [
+                      "rgb(983050)"
+                    ];
+                  };
+                  inactive = {
+                    colors = [
+                      "rgb(1a1625)"
+                    ];
+                  };
+                  locked_active = {
+                    colors = [
+                      "rgb(ccb0ad)"
+                    ];
+                  };
                   locked_inactive = group.groupbar.col.inactive;
                 };
               };
             };
           };
 
-          programs.noctalia-shell = lib.mkIf (cfg.desktops.layers.noctalia.enable) {
+          programs.noctalia-shell = lib.mkIf (cfg.desktops.layers.noctalia-shell.enable) {
             colors = {
               mPrimary = lib.mkForce palette.base0A;
               mSecondary = lib.mkForce palette.base07;
