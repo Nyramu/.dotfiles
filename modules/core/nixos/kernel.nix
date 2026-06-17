9666f908d1fcb561b41e3fbf8fe98659188a7121
Nyramu <nyramu@noreply.codeberg.org>
2026-06-17 18:24:33 +0200

    feat(core/kernel): add scheduler management (services.scx)

--- modules/core/nixos/kernel.nix
+++ modules/core/nixos/kernel.nix
@@ -15,6 +15,14 @@
         nixpkgs.overlays = [ inputs.cachyos-kernel.overlays.pinned ];
         boot.kernelPackages = lib.mkDefault pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto;
 
+        # Run schedulers from userspace
+        services.scx = {
+          enable = true;
+          # scx_lavd: Optimized for gaming, works well for general use cases
+          scheduler = lib.mkDefault "scx_lavd";
+          extraArgs = lib.mkDefault [ "--performance" ];
+        };
+
         # Set the maximum amount of virtual memory areas a single process can map. It's set like on Steam Deck
         boot.kernel.sysctl."vm.max_map_count" = lib.mkForce 2147483642;
       };
