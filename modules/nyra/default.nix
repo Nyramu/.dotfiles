5fe4b2bee3db875cfa7d8538c1e04c29889ad38b
Nyramu <nyramu@noreply.codeberg.org>
2026-06-28 23:39:23 +0200

    feat(nyra): add collector module

---
+++ modules/nyra/default.nix
@@ -0,0 +1,28 @@
+{ self, ... }: {
+
+  flake.modules.homeManager.nyra = {
+    imports = with self.modules.homeManager; [
+      browsers
+      desktops
+      files
+      gaming
+      miscellaneous
+      music
+      productivity
+      services
+      shells
+      socials
+      terminals
+    ];
+  };
+
+  flake.modules.nixos.nyra = {
+    imports = with self.modules.nixos; [
+      desktops
+      gaming
+      login
+      miscellaneous
+      services
+    ];
+  };
+}
