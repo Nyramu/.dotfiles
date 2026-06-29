7fe8bb0a768e9425d43af3eba31206cba95d624a
Nyramu <nyramu@noreply.codeberg.org>
2026-06-29 20:26:08 +0200

    chore(hyprland): remove unused windowrules

--- modules/nyra/desktops/hyprland/windowrules.nix
+++ modules/nyra/desktops/hyprland/windowrules.nix
@@ -1,62 +1,30 @@
-{ lib, ... }:
+{ ... }:
 {
   flake.modules.homeManager.hyprland =
     { config, ... }:
 
     let
       cfg = config.nyra;
+
+      makeFloating = class: {
+        match.class = class;
+        float = true;
+        center = true;
+        min_size = [
+          1280
+          720
+        ];
+        size = [
+          1280
+          720
+        ];
+        dim_around = true;
+      };
     in
     {
-      hyprnix.settings.window_rule =
-        let
-          makeFloating = class: {
-            match.class = class;
-            float = true;
-            center = true;
-            min_size = [
-              1280
-              720
-            ];
-            size = [
-              1280
-              720
-            ];
-            dim_around = true;
-          };
-        in
-        {
-          "floating-viewer" = makeFloating "mpv|imv";
-          "floating-${cfg.terminals.default}" = makeFloating "floating-${cfg.terminals.default}";
-        }
-        // lib.optionalAttrs (cfg.music.rmpc.enable) {
-          "cassette-rmpc" = {
-            match.initial_title = "rmpc";
-            float = true;
-            center = true;
-            min_size = [
-              942
-              558
-            ];
-            size = [
-              942
-              558
-            ];
-          };
-        }
-        // lib.optionalAttrs (cfg.miscellaneous.fastfetch.enable) {
-          "welcome-fastfetch" = {
-            match.initial_title = "fastfetch";
-            float = true;
-            center = true;
-            min_size = [
-              1100
-              736
-            ];
-            size = [
-              1100
-              736
-            ];
-          };
-        };
+      hyprnix.settings.window_rule = {
+        "floating-viewer" = makeFloating "mpv|imv";
+        "floating-${cfg.terminals.default}" = makeFloating "floating-${cfg.terminals.default}";
+      };
     };
 }
