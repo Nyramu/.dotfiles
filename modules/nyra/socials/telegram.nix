d55c4acc40b1082b84e7e4243def6615e7f7d2aa
Nyramu <nyramu@noreply.codeberg.org>
2026-07-27 18:48:47 +0200

    feat(telegram): use chaotic-nyx package by default

--- modules/nyra/socials/telegram.nix
+++ modules/nyra/socials/telegram.nix
@@ -1,13 +1,25 @@
-{ self, lib, ... }:
+{
+  self,
+  inputs,
+  lib,
+  ...
+}:
 {
   flake.modules.homeManager = {
     socials.imports = [ self.modules.homeManager.telegram ];
 
     telegram =
-      { config, pkgs, ... }:
+      {
+        config,
+        pkgs,
+        host,
+        ...
+      }:
 
       let
         cfg = config.nyra.socials.telegram;
+        telegram =
+          inputs.chaotic.packages.${host.system}.telegram-desktop-unwrapped_git or pkgs.telegram-desktop;
       in
       {
         options.nyra.socials.telegram = {
@@ -15,9 +27,7 @@
         };
 
         config = lib.mkIf (cfg.enable) {
-          home.packages = with pkgs; [
-            telegram-desktop
-          ];
+          home.packages = [ telegram ];
 
           hyprnix.settings.bind = lib.mkIf (config.nyra.desktops.hyprland.enable) {
             "SUPER + T".dispatcher.exec_cmd = "Telegram";
