c47c11690267e4096a230771af8d1a9fe1bdda74
Nyramu <nyramu@noreply.codeberg.org>
2026-08-01 13:46:37 +0200

    removal(core/nixos): gc options to preserve outputs and derivations

--- modules/core/nixos/nix.nix
+++ modules/core/nixos/nix.nix
@@ -23,11 +23,6 @@
             # them with hard links to a single copy. This saves disk space.
             auto-optimise-store = true;
 
-            # Prevent garbage collection from altering nix shells managed by nix-direnv
-            # https://github.com/nix-community/nix-direnv#installation
-            keep-outputs = true;
-            keep-derivations = true;
-
             # The maximum number of parallel TCP connections used to fetch files from binary caches and by other downloads.
             # It defaults to 25. 0 means no limit.
             http-connections = 64;
